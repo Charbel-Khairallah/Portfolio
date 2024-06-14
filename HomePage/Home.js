2070ae37f21161b5e0bbcf6e2a63d0545bf07cab
Charbel Khairallah <98479044+Charbel-Khairallah@users.noreply.github.com>
2024-06-14 14:19:44 +0300

    Test Site
    Testing Animations.

---
+++ HomePage/Home.js
@@ -0,0 +1,31 @@
+
+const SocialMediaList = document.getElementById('SocialMediaIconsList');
+const SocialMediaCircles = SocialMediaList.querySelectorAll('*');
+
+SocialMediaCircles.forEach(Circle => {
+    Circle.addEventListener('mouseover', function() {
+        const ChildTags = Circle.querySelectorAll('*');
+        if( ChildTags != null && ChildTags.length > 0){
+            const LowerLevelTags = ChildTags[0].querySelectorAll('*');
+            if(LowerLevelTags != null && LowerLevelTags.length > 0){
+                const SVG = LowerLevelTags[0];
+                SVG.style.animation = 'GlowInAnimation 0.5s forwards';
+            }
+        }
+    });
+
+    
+
+    Circle.addEventListener('mouseout', function() {
+        const ChildTags = Circle.querySelectorAll('*');
+        if( ChildTags != null && ChildTags.length > 0){
+            const LowerLevelTags = ChildTags[0].querySelectorAll('*');
+            if(LowerLevelTags != null && LowerLevelTags.length > 0){
+                const SVG = LowerLevelTags[0];
+                SVG.style.animation = 'GlowOutAnimation 0.5s forwards';
+            }
+        }
+    });
+
+
+});
